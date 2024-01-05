7b1cf315bad36ce1bf76ab304b32efc7c0a5ff71
https://www.linkedin.com/in/jamily-magalh%C3%A3es-da-silva-655a14237/ <magalhaes.jamily@hotmail.com>
2024-01-05 14:28:41 -0400

    feat: MVC-URLs_Amigaveis-PDO

---
+++ Controllers/homeController.php
@@ -0,0 +1,14 @@
+<?php
+
+class homeController extends Controller
+{
+    public function index()
+    {
+        // 1)chamar um model
+        // 2) chamar uma view
+        // 3) fazer junção de back e front
+
+        $model = new Controller;
+        $this->carregarTemplate('home', $model);
+    }
+}
---
+++ README.md
@@ -0,0 +1,2 @@
+# mvc-URLs_Amigaveis-PDO
+Script base para uma aplicação de MVC com implementação de URLs amigáveis com a técnica  "rewrite de URL" ou "rewriting", e extensão PDO.
